be38253460c97170b6e37c8695906bb0ebedfb4c
Cody <codybarker.or@gmail.com>
2023-07-10 14:48:24 -0700

    adding password_digest to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_10_175059) do
+ActiveRecord::Schema.define(version: 2023_07_10_214717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2023_07_10_175059) do
     t.string "profile_image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end

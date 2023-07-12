ad1a2d54605e74a182d277980c1f9acfea8119b4
Cody <codybarker.or@gmail.com>
2023-07-12 15:43:38 -0700

    adding inputs and state management to SignUp component

--- client/src/SignUp.js
+++ client/src/SignUp.js
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function SignUp() {
 
+    const [inputState, setInputState] = useState({
+        username: "",
+        password: "",
+        passwordConfirmation: ""
+    })
+
+    const {
+        username,
+        password,
+        passwordConfirmation
+    } = inputState
+
+    function onInputChange(e){
+        setInputState({
+            ...inputState,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return(
         <div id="signup-container">
-            
+            <form>
+                <label>
+                    Username
+                    <input
+                    name="username"
+                    type="text"
+                    value={username}
+                    onChange={onInputChange}
+                    ></input>
+                </label>
+                <label>
+                    Password
+                    <input
+                    name="password"
+                    type="text"
+                    value={password}
+                    onChange={onInputChange}
+                    ></input>
+                </label>
+                <label>
+                    Password Confirmation
+                    <input
+                    name="passwordConfirmation"
+                    type="text"
+                    value={passwordConfirmation}
+                    onChange={onInputChange}
+                    ></input>
+                </label>
+
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }

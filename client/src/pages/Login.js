1b67828fc72ba8c864b68ef681f82b7027c6d01e
Cody <codybarker.or@gmail.com>
2023-07-13 10:02:23 -0700

    adding login page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+function Login ({ onLogin }) {
+    const [showLogin, setShowLogin] = useState(true)
+
+    return (
+        <div className="wrapper">
+            <h1>TrailScan</h1>
+            {showLogin ? (
+                <>
+                    <LoginForm onLogin={onLogin} />
+                    <hr></hr>
+                    <p>
+                        Don't have an account? &nbsp;
+                        <button onClick={() => setShowLogin(false)}>
+                            Sign Up
+                        </button>
+                    </p>
+                </>
+            ) : (
+                <>
+                    <SignUpForm onLogin={onLogin} />
+                    <hr></hr>
+                    <p>
+                        Already have an account? &nbsp;
+                        <button onClick={() => setShowLogin(true)}>
+                        Log In
+                        </button>
+                    </p>
+                </>
+            )}
+        </div>
+    )
+}
+
+export default Login

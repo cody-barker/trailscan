8c7111c482f06d38c1c512a2248a946c61835933
Cody <codybarker.or@gmail.com>
2023-07-13 11:02:20 -0700

    adding navbar functionality to handle logout

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,31 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-function NavBar() {
+function NavBar({user, setUser}) {
+    function handleLogout() {
+        fetch("/logout", {
+            method: "DELETE"
+        })
+        .then((r) => {
+            if (r.ok) {
+                setUser(null)
+            }
+        })
+    }
+
     return(
         <nav id="navbar">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/signup">Sign Up</NavLink>
+            <h1>
+                <Link to="/">TrailScan</Link>
+            </h1>
+            <nav>
+                <button as={Link} to="/new">
+                    New Trail
+                </button>
+                <button onClick={handleLogout}>
+                    Logout
+                </button>
+            </nav>
         </nav>
     )
 }

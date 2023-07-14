8c8bfdff05926699fc193808cb737f0a84fe5fb9
Cody <codybarker.or@gmail.com>
2023-07-14 09:34:11 -0700

    creating custom methods for number_of_reviews and average_rating for trails

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -9,4 +9,12 @@ class Trail < ApplicationRecord
     validates :family_friendly, inclusion: {in: ["Yes", "No"]}
     validates :trailhead_coordinates, length: {maximum: 39}
 
+    def number_of_reviews
+        self.reviews.count
+    end
+
+    def average_rating
+        self.reviews.average(:trail_rating).round(1)
+    end
+
 end
--- app/serializers/trail_serializer.rb
+++ app/serializers/trail_serializer.rb
@@ -1,5 +1,5 @@
 class TrailSerializer < ActiveModel::Serializer
-  attributes :id, :name, :difficulty, :length, :description, :dogs_allowed, :family_friendly, :trailhead_coordinates, :photo
+  attributes :id, :name, :difficulty, :length, :description, :dogs_allowed, :family_friendly, :trailhead_coordinates, :photo, :number_of_reviews, :average_rating
 
   has_many :reviews
   # has_many :users

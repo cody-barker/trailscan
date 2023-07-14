52c6585cebf5197738fca0582195e556f5c524e1
Cody <codybarker.or@gmail.com>
2023-07-14 10:29:26 -0700

    rendering trail details to TrailDetail comp

--- client/src/pages/TrailDetail.js
+++ client/src/pages/TrailDetail.js
@@ -12,12 +12,24 @@ function TrailDetail( {trails} ) {
     console.log(trail)
 
     return(
+        <>
         <div className="header-container">
             <div className="header-container">
                 <img id="trail-header-photo" src={trail.photo} alt={trail.name}></img>
             </div>
             <div className="trail-header-text">{trail.name}</div>
         </div>
+        <div className="trail-details-container">
+            <p>Length: {trail.length}</p>
+            <p>Difficulty: {trail.difficulty}</p>
+            <p>Trailhead Coordinates: {trail.trailhead_coordinates}</p>
+            <p>Family Friendly: {trail.family_friendly}</p>
+            <p>Dogs Allowed: {trail.dogs_allowed}</p>
+            <h3>Description</h3>
+            <p>{trail.description}</p>
+            <h3>Reviews</h3>
+        </div>
+        </>
     )
 }
 

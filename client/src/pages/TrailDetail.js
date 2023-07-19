53f152828e484a68385a8d8f9c8b8f1ef7449714
Cody <codybarker.or@gmail.com>
2023-07-19 11:50:22 -0700

    formatting trail detail page

--- client/src/pages/TrailDetail.js
+++ client/src/pages/TrailDetail.js
@@ -23,30 +23,32 @@ function TrailDetail() {
             </div>
             <div className="trail-header-text">{trail.name}</div>
         </div>
-    
-        <div className="trail-details-container">
-            <span className="detail">Length: {trail.length} mi</span>
-            <span className="detail">Difficulty: {trail.difficulty}</span>
-            <span className="detail">Family Friendly: {trail.family_friendly}</span>
-            <span className="detail">Dogs Allowed: {trail.dogs_allowed}</span>
-            <p>Trailhead Coordinates: {trail.trailhead_coordinates}</p>
-            <h3>Description</h3>
-            <p>{trail.description}</p>
-        </div>
-            <div className="reviews">
-                <h3>Reviews</h3>
-                <NavLink to={`/trails/${id}/reviews`}>
-                    <button className="green-btn">Write Review</button>
-                </NavLink>
-                {trail? (
-                    trail.reviews.sort((a,b) => (a.date < b.date) ? 1 : -1).map((review) => {
-                        return <TrailReview key={review.id} review={review}/>
-                    })
-                ) : (
-                    <p>"Loading..."</p>
-                )}
+
+        <div className="trail-page-container">
+            <div className="trail-details-container">
+                <span className="detail"><b>Length:</b> {trail.length} mi</span>
+                <span className="detail"><b>Difficulty:</b> {trail.difficulty}</span>
+                <span className="detail"><b>Family Friendly:</b> {trail.family_friendly}</span>
+                <span className="detail"><b>Dogs Allowed: </b>{trail.dogs_allowed}</span>
+                <p className="coordinates"><b>Trailhead Coordinates: </b>{trail.trailhead_coordinates}</p>
+                <h3>Description</h3>
+                <p>{trail.description}</p>
             </div>
-        
+                <div className="reviews">
+                    <h3>Reviews</h3>
+                    <NavLink to={`/trails/${id}/reviews`}>
+                        <button className="green-btn">Write Review</button>
+                    </NavLink>
+                    <hr></hr>
+                    {trail? (
+                        trail.reviews.sort((a,b) => (a.date < b.date) ? 1 : -1).map((review) => {
+                            return <TrailReview key={review.id} review={review}/>
+                        })
+                    ) : (
+                        <p>"Loading..."</p>
+                    )}
+            </div>
+        </div>
         </>
     )
 }

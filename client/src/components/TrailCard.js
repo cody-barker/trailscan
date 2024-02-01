3b52cb4a2fa09bf4f0b3214c52a6107f20203cc1
Cody <codybarker.or@gmail.com>
2024-02-01 14:47:50 -0800

    set trail img class to trail__img

--- client/src/components/TrailCard.js
+++ client/src/components/TrailCard.js
@@ -1,32 +1,31 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 function TrailCard({ trail }) {
+  const {
+    id,
+    name,
+    difficulty,
+    length,
+    photo,
+    number_of_reviews,
+    average_rating,
+  } = trail;
 
-    const {
-        id,
-        name,
-        difficulty,
-        length,
-        photo,
-        number_of_reviews,
-        average_rating
-    } =  trail
-
-    return(
-        <div className="trail-card">
-            <NavLink to={`/trails/${id}`}>
-            <img src={photo} alt="trail"></img>
-            <span>{difficulty}</span>
-            <span> • </span>
-            <span id="star">{'\u2605'}</span>
-            <span> {average_rating}</span>
-            <span className="grey"> ({number_of_reviews})</span>
-            <h3>{name}</h3>
-            <p>Length: {length} mi</p>
-            </NavLink>
-        </div>
-    )
+  return (
+    <div className="trail-card">
+      <NavLink to={`/trails/${id}`}>
+        <img src={photo} className="trail__img" alt="trail"></img>
+        <span>{difficulty}</span>
+        <span> • </span>
+        <span id="star">{"\u2605"}</span>
+        <span> {average_rating}</span>
+        <span className="grey"> ({number_of_reviews})</span>
+        <h3>{name}</h3>
+        <p>Length: {length} mi</p>
+      </NavLink>
+    </div>
+  );
 }
 
-export default TrailCard
+export default TrailCard;

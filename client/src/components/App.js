616411e0260671eb196ffa9a0af938b207aac528
Cody <codybarker.or@gmail.com>
2024-02-01 14:31:11 -0800

    add border box styling, move NavBar into header at top of App.js, clear comments in index.html

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,51 +1,37 @@
-import { useContext } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import NavBar from './NavBar'
-import Login from '../pages/Login'
-import Trails from '../pages/Trails'
-import TrailDetail from '../pages/TrailDetail'
-import { UserContext } from '../contexts/UserContext'
-import { TrailsProvider } from '../contexts/TrailsContext'
-import UserProfile from '../pages/UserProfile'
-import ReviewEdit from '../pages/ReviewEdit'
-import ReviewCreate from '../pages/ReviewCreate'
-import TrailCreate from '../pages/TrailCreate'
-import '../styles/App.css';
+import { useContext } from "react";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import Login from "../pages/Login";
+import Trails from "../pages/Trails";
+import TrailDetail from "../pages/TrailDetail";
+import { UserContext } from "../contexts/UserContext";
+import { TrailsProvider } from "../contexts/TrailsContext";
+import UserProfile from "../pages/UserProfile";
+import ReviewEdit from "../pages/ReviewEdit";
+import ReviewCreate from "../pages/ReviewCreate";
+import TrailCreate from "../pages/TrailCreate";
+import "../styles/App.css";
 
 function App() {
-  const {user} = useContext(UserContext)
+  const { user } = useContext(UserContext);
   if (!user) {
-    return <Login />
+    return <Login />;
   }
 
   return (
     <TrailsProvider>
-      <main>
+      <header className="header">
         <NavBar />
-          <Routes>
-            <Route 
-              path="/user/:uid/reviews/:id/edit"
-              element={<ReviewEdit />}
-            />
-            <Route
-              path="/user/:id/reviews"
-              element={<UserProfile />}
-            />
-            <Route
-              path="/trails/:id/reviews"
-              element={<ReviewCreate />}
-            />
-            <Route
-              path="/trails/:id"
-              element={<TrailDetail />}/>
-            <Route 
-              path="/trails"
-              element={<TrailCreate />}
-            />
-            <Route 
-              path="/" 
-              element={<Trails />}/>
-          </Routes>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/user/:uid/reviews/:id/edit" element={<ReviewEdit />} />
+          <Route path="/user/:id/reviews" element={<UserProfile />} />
+          <Route path="/trails/:id/reviews" element={<ReviewCreate />} />
+          <Route path="/trails/:id" element={<TrailDetail />} />
+          <Route path="/trails" element={<TrailCreate />} />
+          <Route path="/" element={<Trails />} />
+        </Routes>
       </main>
     </TrailsProvider>
   );

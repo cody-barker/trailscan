d8bd296dd7e7a414c0875252379d50ac84f5069d
Cody Barker <codybarker.or@gmail.com>
2024-02-02 13:03:37 -0800

    add footer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,6 +10,7 @@ import UserProfile from "../pages/UserProfile";
 import ReviewEdit from "../pages/ReviewEdit";
 import ReviewCreate from "../pages/ReviewCreate";
 import TrailCreate from "../pages/TrailCreate";
+import Footer from "../components/Footer"
 import "../styles/App.css";
 
 function App() {
@@ -33,6 +34,7 @@ function App() {
           <Route path="/" element={<Trails />} />
         </Routes>
       </main>
+      <Footer />
     </TrailsProvider>
   );
 }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,18 @@
+
+
+function Footer() {
+    return(
+        <footer>
+            <div className="footer__container container">
+                <div className="footer--left">
+                    <h2>TrailScan</h2>
+                </div>
+                <div className="footer--right">
+                    <p>Cody Barker 2024</p>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer

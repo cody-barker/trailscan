5f1b7f3a7e26031fccedb0c577d9223c2c9b5006
Cody <codybarker.or@gmail.com>
2024-02-01 16:29:09 -0800

    style trail detail page to be responsive

--- client/src/pages/TrailDetail.js
+++ client/src/pages/TrailDetail.js
@@ -13,54 +13,71 @@ function TrailDetail() {
         return <p>"Loading..."</p>
     }
 
-    return(
-        <>
+    return (
+      <>
         <div className="header-container">
-            <div className="header-container">
-                <img id="trail-header-photo" src={trail.photo} alt={trail.name}></img>
-            </div>
-            <div className="trail-header-text">{trail.name}</div>
+          <div className="header-container">
+            <img
+              id="trail-header-photo"
+              src={trail.photo}
+              alt={trail.name}
+            ></img>
+          </div>
+          <div className="trail-header-text">{trail.name}</div>
         </div>
 
         <div className="trail-page-container">
-            <div className="trail-details-container">
-                <span className="detail"><b>Length:</b> {trail.length} mi</span>
-                <span className="detail"><b>Difficulty:</b> {trail.difficulty}</span>
-                <span className="detail"><b>Family Friendly:</b> {trail.family_friendly}</span>
-                <span className="detail"><b>Dogs Allowed: </b>{trail.dogs_allowed}</span>
-            </div>
-            <div className="trail-map-container">
+          <div className="trail-details-container">
+            <span className="detail">
+              <b>Length:</b> {trail.length} mi
+            </span>
+            <span className="detail">
+              <b>Difficulty:</b> {trail.difficulty}
+            </span>
+            <span className="detail">
+              <b>Family Friendly:</b> {trail.family_friendly}
+            </span>
+            <span className="detail">
+              <b>Dogs Allowed: </b>
+              {trail.dogs_allowed}
+            </span>
+          </div>
+          <div className="trail-map-container">
             <h4>Description</h4>
             <p>{trail.description}</p>
-                <iframe
-                    title="trailhead-map"
-                    width="600"
-                    height="450"
-                    style={{border:0}}
-                    loading="lazy"
-                    allowFullScreen
-                    referrerPolicy="no-referrer-when-downgrade"
-                    src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyC97JxmBrPGPb6Pl2DWTcmsvUEIg_I4pkM
-                        &q=${trail.trailhead_coordinates}&zoom=13`}>
-                </iframe>
+            <div className="iframe-wrapper">
+              <iframe
+                title="trailhead-map"
+                width="600"
+                height="450"
+                style={{ border: 0 }}
+                loading="lazy"
+                allowFullScreen
+                referrerPolicy="no-referrer-when-downgrade"
+                src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyC97JxmBrPGPb6Pl2DWTcmsvUEIg_I4pkM
+                        &q=${trail.trailhead_coordinates}&zoom=13`}
+              ></iframe>
             </div>
-    
-            <div className="reviews">
-                <h3>Reviews</h3>
-                <NavLink to={`/trails/${id}/reviews`}>
-                    <button className="green-btn">Write Review</button>
-                </NavLink>
-                {trail ? (
-                    trail.reviews.sort((a,b) => (a.date < b.date) ? 1 : -1).map((review) => {
-                        return <TrailReview key={review.id} review={review}/>
-                    })
-                ) : (
-                    <p>"Loading..."</p>
-                )}
-            </div>
+          </div>
+
+          <div className="reviews">
+            <h3>Reviews</h3>
+            <NavLink to={`/trails/${id}/reviews`}>
+              <button className="green-btn">Write Review</button>
+            </NavLink>
+            {trail ? (
+              trail.reviews
+                .sort((a, b) => (a.date < b.date ? 1 : -1))
+                .map((review) => {
+                  return <TrailReview key={review.id} review={review} />;
+                })
+            ) : (
+              <p>"Loading..."</p>
+            )}
+          </div>
         </div>
-        </>
-    )
+      </>
+    );
 }
 
 export default TrailDetail
